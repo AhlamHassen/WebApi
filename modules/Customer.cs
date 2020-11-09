ab4e76f350dd9315297b9456eff2dbae090b166c
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-11-09 21:47:11 +1100

    Inserting in2 table & finding customers Endpoints

--- modules/Customer.cs
+++ modules/Customer.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace modules
 {
     public class Customer
     {
+        [JsonProperty("ID")]
         public int ID { get; set; } 
+
+        [JsonProperty("FirstName")]
         public string FirstName { get; set; }
+
+        [JsonProperty("LastName")]
         public string LastName { get; set; }
+
+        public Customer(){
+            this.FirstName = "unknown";
+            this.LastName = "unknown";
+        }
 
         public Customer(int id, string firsN, string LastN){
             this.ID = id;
--- webApiFolder/Controllers/SqlController.cs
+++ webApiFolder/Controllers/SqlController.cs
@@ -57,9 +57,84 @@ namespace webApi
                 return "can not delete" + sq.Message;
             }
             
+        }
 
+        [HttpPost("Insert")]
+        public string addCustomer([FromBody]Customer c){
+             string connectionString = @"Data Source=bikestoresdb.c3raologixkl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=SampleDB;User ID=admin; Password=abcd1234";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            //recieved this error => Cannot insert explicit value for identity column in table 'Customer' when IDENTITY_INSERT is set to OFF.
+            string queryString = "insert into Customer (FirstName, LastName, City, Country, Phone)"; 
+            queryString += "Values (@fname, @lname, 'keren', 'Eritrea', '12345')";
+
+            SqlCommand command = new SqlCommand(queryString, con);
+            // command.Parameters.AddWithValue("@Id", c.ID);
+            command.Parameters.AddWithValue("@fname", c.FirstName);
+            command.Parameters.AddWithValue("@lname", c.LastName);
+
+
+            con.Open();
+            try{
+                var result = command.ExecuteNonQuery();
+                return result.ToString();
+            }catch(SqlException se){
+                return se.Message;
+            }   
+        }
+
+        [HttpGet("GetCustomer")]
+        public List<Customer> getCustomer(){
+            string connectionString = @"Data Source=bikestoresdb.c3raologixkl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=SampleDB;User ID=admin; Password=abcd1234";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string queryString = "select * from Customer";
+
+            SqlCommand command = new SqlCommand(queryString, con);
+            // command.Parameters.AddWithValue("@lname", c.LastName);
+            con.Open();
             
+            using(SqlDataReader reader = command.ExecuteReader()){
+                while(reader.Read())
+                {
+                    customers.Add(
+                        new Customer((int)reader[0], reader[1].ToString(), reader[2].ToString())
+                    );
+                }
+            }
+
+            return customers;
+
+        }
+
+        [HttpPost("find")]
+        public List<Customer> findcustomer([FromBody] Customer c){
+            string connectionString = @"Data Source=bikestoresdb.c3raologixkl.us-east-1.rds.amazonaws.com;
+            Initial Catalog=SampleDB;User ID=admin; Password=abcd1234";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            string queryString = "select * from Customer where LastName=@lname";
+
+            SqlCommand command = new SqlCommand(queryString, con);
+            command.Parameters.AddWithValue("@lname", c.LastName);
+            con.Open();
             
+            using(SqlDataReader reader = command.ExecuteReader()){
+                while(reader.Read())
+                {
+                    customers.Add(
+                        new Customer((int)reader[0], reader[1].ToString(), reader[2].ToString())
+                    );
+                }
+            }
+
+            return customers;
+
         }
         
     }
